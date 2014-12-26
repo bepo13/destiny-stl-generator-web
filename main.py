30a7ade8766275a3b9734c05f599cda27c3950b8
bepo13 <bepo13@gmail.com>
2014-12-26 17:45:08 -0600

    debug

--- main.py
+++ main.py
@@ -18,27 +18,23 @@ def welcome():
 def generate():
     item = request.args.get('item')
 
-    fi = open(gearFile, 'r')
-    return fi.read()
-    
-    # Download the model data for this item
-    try:
-        model = DestinyModel(item, gear[item.lower()])
-        output = model.generate()
-    except:
-        # output = "Unable to find requested item: "+item
-        output = json.dumps(gear)
-        
-    # Return the stl output or response
-    return render_template('output.html', output=output)
-    
-if __name__ == '__main__':
     # Load gear JSON file
     fi = open(gearFile, 'r')
     gear = json.loads(fi.read())
     gear = {k.lower():v for k,v in gear.items()}
     fi.close()
     
+    # Download the model data for this item
+    try:
+        model = DestinyModel(item, gear[item.lower()])
+        output = model.generate()
+    except:
+        output = "Unable to find requested item: "+item
+        
+    # Return the stl output or response
+    return render_template('output.html', output=output)
+    
+if __name__ == '__main__':
     # Run Flask
     app.debug = True
     app.run()
