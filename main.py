d18669e4c35650a6f4928ec75296b9bd18894204
bepo13 <bepo13@gmail.com>
2015-01-07 18:22:55 -0600

    Added download page and cleaned up files
    Replaced generate page with download page

--- main.py
+++ main.py
@@ -19,42 +19,55 @@ def welcome():
     
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html') 
     
 @app.route('/generate', methods=['GET'])
 def generate():
+    # Parse the arguments for item name and generate the key
     item = request.args.get('item')
     key = item.replace('[',"").replace(']',"").replace('\'',"").lower()
-
-    # Load gear JSON file
-    f = open("./gear/gear.json", 'r')
-    gear = json.loads(f.read())
-    f.close()
     
-    # Download the model data for this item
-    try:
-        model = DestinyModel(item, gear[key]["json"])
-        output = model.generate()
-    except:
-        output = "Unable to find requested item: "+str(item)
+    # Create stl file name and path
+    fileName = key.replace(" ","_")+".stl"
+    filePath = outputPath+fileName
+    
+    # Create output directory
+    if not os.path.exists(outputPath):
+        os.makedirs(outputPath)
+    
+    # Check if file has already been created
+    if os.path.exists(filePath):
+        print("Using existing file "+filePath)
+        # Read in the file contents
+        with open(filePath, 'r') as fi:
+            output = fi.read()
+            fi.close()
     else:
+        # Load gear JSON file
+        f = open("./gear/gear.json", 'r')
+        gear = json.loads(f.read())
+        f.close()
+        
+        # Download the model data for this item
         try:
-            # Create temp directory
-            if not os.path.exists(outputPath):
-                os.makedirs(outputPath)
-                
-            # Write temp file if it does not already exist
-            filePath = outputPath+key+".stl"
-            if not os.path.exists(filePath):
-                with open(filePath, 'w') as fo:
-                    fo.write(output)
-                    fo.close()
+            # Download the model geometries
+            model = DestinyModel(item, gear[key]["json"])
+            
+            # Generate the output
+            output = model.generate()
+            
+            # Write output file
+            with open(filePath, 'w') as fo:
+                fo.write(output)
+                fo.close()
             print("Wrote output file "+filePath)
         except:
-            print("Unable to create file "+filePath)
-        
-    # Return the stl output or response
-    return render_template('output.html', output=output)
+            output = "Unable to generate files for item: "+str(item)
+            return render_template('output.html', output=output)
+            # error page
+            
+    # Return the file download page
+    return render_template('download.html', item=item, fileName=fileName, filePath=filePath, output=output)
 
 @app.route('/stl/<path:filename>')
 def send_tmp_file(filename):
